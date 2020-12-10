f3d09520b5f5813bf6b5019d0529104f65b1523a
JonasJoaquim <72474952+JonasJoaquim@users.noreply.github.com>
2020-12-09 23:12:51 -0300

    Add files via upload

---
+++ 01_Trabalho/Codigo/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <gl/glew.h>
+#include <GL/glut.h>
+#include <stdio.h>
+#include <windows.h>
+#include <errno.h>
+
+#endif // MAIN_H_INCLUDED
